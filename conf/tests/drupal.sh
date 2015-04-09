bc71c656ae3ab6c85fbec2cd4529701c86931634
zaporylie <jakub@piaseccy.pl>
2015-04-09 21:46:43 +0200

    Add drupal.sh

---
+++ conf/tests/drupal.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+handleSigTerm()
+{
+  echo SIGTERM
+}
+
+oneTimeSetUp()
+{
+  trap "handleSigTerm" TERM
+}
+
+testDrupal()
+{
+  if [ "${DRUPAL_TEST}" = 1 ]; then
+    cd /app/drupal/sites/${DRUPAL_SUBDIR}
+    drush en simpletest -y
+    drush test-run --all
+  else
+    startSkipping
+  fi
+  assertEquals "Drupal test failed" 0 ?$
+}
+
+. shunit2
