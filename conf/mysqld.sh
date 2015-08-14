82746ebd629ca1918645819acce794f3902a2e8c
root <jakub@piaseccy.pl>
2015-08-14 07:42:49 -0400

    Probably that's the problem - not enough time between server start and password set

--- conf/mysqld.sh
+++ conf/mysqld.sh
@@ -5,7 +5,7 @@ if [ "$(cat /etc/hosts | grep ${MYSQL_HOST_NAME} | wc -l)" = "0" ]; then
   echo "Unable to find linked mysql container, running mysql on host"
 
   /usr/bin/mysqld_safe --skip-syslog &
-  sleep 3s
+  sleep 6s
   export MYSQL_HOST_NAME=localhost
   export MYSQL_ENV_MYSQL_ROOT_PASSWORD=`pwgen -c -n -1 12`
   export MYSQL_PORT_3306_TCP_PROTO=tcp
@@ -14,7 +14,7 @@ if [ "$(cat /etc/hosts | grep ${MYSQL_HOST_NAME} | wc -l)" = "0" ]; then
   export MYSQL_PORT=tcp://127.0.0.1:3306
   mysqladmin -u root password $MYSQL_ENV_MYSQL_ROOT_PASSWORD
   killall mysqld
-  sleep 3s
+  sleep 6s
 
   /etc/init.d/mysql start
 fi
