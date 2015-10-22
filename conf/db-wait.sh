93b18f72739cfeb72ec6fad963c3417b834b22f4
Jakub Piasecki <jakub@nymedia.no>
2015-10-22 09:59:21 +0200

    Show 'waiting for mysql' message better way

--- conf/db-wait.sh
+++ conf/db-wait.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
-while ! mysql -h${MYSQL_HOST_NAME} -p${MYSQL_ENV_MYSQL_ROOT_PASSWORD}  -e ";" ; do
-  sleep 0.5
-done
+WaitForMySQL ()
+{
+    LOOP_LIMIT=60
+    for (( i=0 ; ; i++ )); do
+        if [ ${i} -eq ${LOOP_LIMIT} ]; then
+            echo "Time out."
+            exit 1
+        fi
+        echo "=> Waiting for confirmation of MySQL service startup, trying ${i}/${LOOP_LIMIT} ..."
+        sleep 1
+        mysql -h${MYSQL_HOST_NAME} -p${MYSQL_ENV_MYSQL_ROOT_PASSWORD}  -e ";" > /dev/null 2>&1 && break
+    done
+}
+
+WaitForMySQL
